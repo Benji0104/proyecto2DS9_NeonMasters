ba2fb5ce183de2004bbc0146258f986ce31af19a
benjamingildese82@gmail.com <Xion507@gmail.com>
2025-06-23 15:13:05 -0500

    Bugs

--- Pantallas/ca_seguridad.php
+++ Pantallas/ca_seguridad.php
@@ -1,6 +1,6 @@
+<?php
 // Verifica si hay una sesión activa; redirige al usuario a la página de inicio si no está autenticado.
 // Checks if a session is active; redirects the user to the homepage if not authenticated.
-<?php
 session_start();
 
 if (!isset($_SESSION['user'])) {
